8b56156374d9e98a2d8dafe38bd039929755b3ff
salima12345 <elkhalidi.s.fst@uhp.ac.ma>
2024-11-04 12:04:24 +0100

    save

--- src/components/ui/AnimetedLayout.tsx
+++ src/components/ui/AnimetedLayout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/layout/footer/Footer";
 export default function AnimatedLayout({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ y: 40, opacity: 0 }}
+      initial={{ y: 50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.75 }}
     >
